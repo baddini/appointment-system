74cb85d309e9276441642a62946d0edf843353de
exk0730@gmail.com <exk0730@gmail.com@80e67094-1e62-00f2-85e5-5759996c6595>
2011-04-25 20:14:05 +0000

    Doctor object complete. Also refactored the password field of doctor out, like patient.

--- trunk/src/edu/rit/rdi/as/datalayer/Doctor.java
+++ trunk/src/edu/rit/rdi/as/datalayer/Doctor.java
@@ -1,9 +1,15 @@
 package edu.rit.rdi.as.datalayer;
 
+import java.util.ArrayList;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.HashMap;
 
+import static edu.rit.rdi.as.datalayer.Tables.*;
+
 /**
+ * POJO for Doctor table.
+ * This class exposes methods to deal with CRUD operations on a Doctor object.
  * @date Apr 24, 2011
  * @author Eric Kisner
  */
@@ -16,14 +22,13 @@ public class Doctor extends AbstractDatabasePOJO {
     private String email;
     private String phone;
     private String username;
-    private String password;
 
     public Doctor() {
         super();
     }
 
     public Doctor( int doctorId, String firstName, String lastName, String gender, String email, String phone,
-                   String username, String password ) {
+                   String username ) {
         this();
         this.doctorId = doctorId;
         this.firstName = firstName;
@@ -32,9 +37,7 @@ public class Doctor extends AbstractDatabasePOJO {
         this.email = email;
         this.phone = phone;
         this.username = username;
-        this.password = password;
     }
-
 
     public Doctor( int doctorId ) {
         this();
@@ -61,10 +64,6 @@ public class Doctor extends AbstractDatabasePOJO {
         return lastName;
     }
 
-    public String getPassword() {
-        return password;
-    }
-
     public String getPhone() {
         return phone;
     }
@@ -81,35 +80,126 @@ public class Doctor extends AbstractDatabasePOJO {
                + "\tgender=" + gender
                + "\temail=" + email
                + "\tphone=" + phone
-               + "\tusername=" + username
-               + "\tpassword=" + password + '}';
+               + "\tusername=" + username + '}';
     }
 
     public boolean put() throws SQLException {
-        throw new UnsupportedOperationException( "Not supported yet." );
+        if( fetch() == null ) {
+            String sql = "INSERT INTO " + Doctor + "(" + asColumns() + ") VALUES ("
+                         + doctorId + ","
+                         + firstName + ","
+                         + lastName + ","
+                         + gender + ","
+                         + email + ","
+                         + phone + ","
+                         + username + "," + ")";
+            conn.executeUpdateQuery( sql );
+            return true;
+        } else {
+            return post();
+        }
     }
 
     public Object fetch( int primaryKeyId ) throws SQLException {
-        throw new UnsupportedOperationException( "Not supported yet." );
+        String sql = "SELECT * FROM " + Doctor + " WHERE doctor_id = " + primaryKeyId;
+        ResultSet rs = conn.executeQuery( sql );
+        if( buildThisDoctor( conn.getSingleRow( rs ) ) ) {
+            return this;
+        }
+        return null;
     }
 
     public Object fetch() throws SQLException {
-        throw new UnsupportedOperationException( "Not supported yet." );
+        return fetch( doctorId );
     }
 
     public boolean post() throws SQLException {
-        throw new UnsupportedOperationException( "Not supported yet." );
+        int executeUpdateQuery = -1;
+        //We aren't updating the primary key of this table, so we don't want to include it in the map we receive.
+        HashMap<String, String> columnsToData = asMap( false );
+        for( String key : columnsToData.keySet() ) {
+            String sql = "UPDATE " + Doctor + " SET " + key + " = '" + columnsToData.get( key ) + "'"
+                         + " WHERE doctor_id = " + doctorId;
+            executeUpdateQuery = conn.executeUpdateQuery( sql );
+        }
+        if( executeUpdateQuery < 0 ) {
+            return false;
+        }
+        return true;
     }
 
     public boolean delete() throws SQLException {
-        throw new UnsupportedOperationException( "Not supported yet." );
+        //There are no appointments for this doctor - we can delete the doctor information
+        if( new Appointment( doctorId, "garbage" ).fetch() == null ) {
+            String sql = "DELETE FROM " + Doctor + " WHERE doctor_id = " + doctorId;
+            int executeUpdateQuery = conn.executeUpdateQuery( sql );
+            if( executeUpdateQuery > 0 ) {
+                return true;
+            }
+        }
+        return false;
     }
 
     public boolean fullDelete() throws SQLException {
-        throw new UnsupportedOperationException( "Not supported yet." );
+        //First check if we can just delete this doctor outright.
+        if( delete() ) {
+            return true;
+        } else {
+            //If we could not delete the doctor, delete any appointments this doctor may have.
+            String sql = "DELETE FROM " + Appointment + " WHERE doctor_id = " + doctorId;
+            int executeUpdateQuery = conn.executeUpdateQuery( sql );
+            if( executeUpdateQuery > 0 ) {
+                //Now that we have deleted all associated children of this doctor, we should be able to delete
+                //the doctor's information.
+                return delete();
+            }
+        }
+        return false;
     }
 
     public HashMap asMap( boolean includePrimaryKey ) {
-        throw new UnsupportedOperationException( "Not supported yet." );
+        HashMap<String, String> map = new HashMap<String, String>();
+        if( includePrimaryKey ) {
+            map.put( "doctor_id", String.valueOf( doctorId ) );
+        }
+        map.put( "firstName", firstName );
+        map.put( "lastName", lastName );
+        map.put( "gender", gender );
+        map.put( "email", email );
+        map.put( "phone", phone );
+        map.put( "username", username );
+        return map;
+    }
+
+    /**
+     * Returns this object's fields as a String of comma-separated values.
+     */
+    private static String asColumns() {
+        return "doctor_id, firstName, lastName, gender, email, phone, username";
+    }
+
+    /**
+     * Populates this Doctor object with the specified data. The data must contain all fields of the table
+     * we are pulling from.
+     * @param data The data from a "SELECT *" statement.
+     * @return True if the given data is: not null and correctly populates this Doctor object, else false.
+     */
+    private boolean buildThisDoctor( ArrayList<String> data ) {
+        if( data == null || data.isEmpty() || data.size() < 7 ) {
+            return false;
+        }
+        try {
+            this.doctorId = Integer.valueOf( data.get( 0 ) );
+        } catch( NumberFormatException ignore ) {
+            return false;
+        }
+        this.firstName = data.get( 1 );
+        this.lastName = data.get( 2 );
+        this.gender = data.get( 3 );
+        this.email = data.get( 4 );
+        this.phone = data.get( 5 );
+        this.username = data.get( 6 );
+
+        return true;
     }
 }
