797d10c7f949a59dabbf2108a309971084b6afe1
LunarZeal <LunarZeal@gmail.com>
2011-05-05 21:02:25 +0000

    Updated DesktopClient with base connection code.

--- DesktopClient/Calender System/Calender System/LoginView.Designer.cs
+++ DesktopClient/Calender System/Calender System/LoginView.Designer.cs
@@ -38,34 +38,39 @@
             // 
             // txtName
             // 
-            this.txtName.Location = new System.Drawing.Point(100, 70);
+            this.txtName.Location = new System.Drawing.Point(133, 86);
+            this.txtName.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.txtName.Name = "txtName";
-            this.txtName.Size = new System.Drawing.Size(112, 20);
+            this.txtName.Size = new System.Drawing.Size(148, 22);
             this.txtName.TabIndex = 0;
             // 
             // txtPass
             // 
-            this.txtPass.Location = new System.Drawing.Point(100, 100);
+            this.txtPass.Location = new System.Drawing.Point(133, 123);
+            this.txtPass.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.txtPass.Name = "txtPass";
             this.txtPass.PasswordChar = '*';
-            this.txtPass.Size = new System.Drawing.Size(112, 20);
+            this.txtPass.Size = new System.Drawing.Size(148, 22);
             this.txtPass.TabIndex = 1;
             // 
             // btnLogin
             // 
-            this.btnLogin.Location = new System.Drawing.Point(75, 170);
+            this.btnLogin.Location = new System.Drawing.Point(100, 209);
+            this.btnLogin.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.btnLogin.Name = "btnLogin";
-            this.btnLogin.Size = new System.Drawing.Size(100, 38);
+            this.btnLogin.Size = new System.Drawing.Size(133, 47);
             this.btnLogin.TabIndex = 2;
             this.btnLogin.Text = "Login";
             this.btnLogin.UseVisualStyleBackColor = true;
+            this.btnLogin.Click += new System.EventHandler(this.btnLogin_Click);
             // 
             // lblTitle
             // 
             this.lblTitle.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblTitle.Location = new System.Drawing.Point(50, 0);
+            this.lblTitle.Location = new System.Drawing.Point(67, 0);
+            this.lblTitle.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.lblTitle.Name = "lblTitle";
-            this.lblTitle.Size = new System.Drawing.Size(150, 37);
+            this.lblTitle.Size = new System.Drawing.Size(200, 46);
             this.lblTitle.TabIndex = 3;
             this.lblTitle.Text = "Login";
             this.lblTitle.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
@@ -73,32 +78,35 @@
             // lblName
             // 
             this.lblName.AutoSize = true;
-            this.lblName.Location = new System.Drawing.Point(30, 70);
+            this.lblName.Location = new System.Drawing.Point(40, 86);
+            this.lblName.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.lblName.Name = "lblName";
-            this.lblName.Size = new System.Drawing.Size(58, 13);
+            this.lblName.Size = new System.Drawing.Size(77, 17);
             this.lblName.TabIndex = 4;
             this.lblName.Text = "Username:";
             // 
             // lblPass
             // 
             this.lblPass.AutoSize = true;
-            this.lblPass.Location = new System.Drawing.Point(30, 100);
+            this.lblPass.Location = new System.Drawing.Point(40, 123);
+            this.lblPass.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.lblPass.Name = "lblPass";
-            this.lblPass.Size = new System.Drawing.Size(56, 13);
+            this.lblPass.Size = new System.Drawing.Size(73, 17);
             this.lblPass.TabIndex = 5;
             this.lblPass.Text = "Password:";
             // 
             // LoginView
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(234, 212);
+            this.ClientSize = new System.Drawing.Size(312, 261);
             this.Controls.Add(this.lblPass);
             this.Controls.Add(this.lblName);
             this.Controls.Add(this.lblTitle);
             this.Controls.Add(this.btnLogin);
             this.Controls.Add(this.txtPass);
             this.Controls.Add(this.txtName);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "LoginView";
             this.Text = "Login";
             this.ResumeLayout(false);
--- DesktopClient/Calender System/Calender System/LoginView.cs
+++ DesktopClient/Calender System/Calender System/LoginView.cs
@@ -7,13 +7,64 @@ using System.Linq;
 using System.Text;
 using System.Windows.Forms;
 
+using System.Net;
+using System.IO;
+using System.Xml;
+
 namespace Calender_System
 {
     public partial class LoginView : Form
     {
+        private String baseURI = "http://simon.ist.rit.edu:8080/BeerService/resources/Services/Beers/";
+        
         public LoginView()
         {
             InitializeComponent();
+        }
+
+        private void btnLogin_Click(object sender, EventArgs e)
+        {
+            ServiceMethod("GET", baseURI + "Costliest");
+        }
+
+        //Find the cheapest beer
+        void ServiceMethod(String methodType, String uri)
+        {
+            try
+            {
+                HttpWebRequest request = (HttpWebRequest)WebRequest.Create(uri);
+                request.Method = methodType;
+                HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+                txtName.Text = ConvertResponse(response);
+                txtPass.Text = "";
+            }
+            catch (Exception)
+            {
+                txtName.Text = "ERROR: Could not connect to service.";
+                txtPass.Text = "";
+            }
+        }
+
+        //Converts xml of response stream into string 
+        String ConvertResponse(HttpWebResponse response)
+        {
+            String outputString = "";
+            String result = new StreamReader(response.GetResponseStream()).ReadToEnd();
+            using (XmlReader reader = XmlReader.Create(new StringReader(result)))
+            {
+                XmlWriterSettings ws = new XmlWriterSettings();
+                // Parse the file and display each of the nodes.
+                while (reader.Read())
+                {
+                    switch (reader.NodeType)
+                    {
+                        case XmlNodeType.Text:
+                            outputString += reader.Value + "\n";
+                            break;
+                    }
+                }
+            }
+            return outputString;
         }
     }
 }
