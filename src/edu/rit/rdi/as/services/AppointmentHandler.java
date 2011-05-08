f774a5b179aa0f836f52c85a760fa460a2aff288
exk0730 <exk0730@gmail.com>
2011-05-08 15:29:09 +0000

    Implemented "changeAppointment" in the Appointment Handler. Now just need to expose the method in the service layer.

--- src/edu/rit/rdi/as/services/AppointmentHandler.java
+++ src/edu/rit/rdi/as/services/AppointmentHandler.java
@@ -3,6 +3,7 @@ package edu.rit.rdi.as.services;
 import edu.rit.rdi.as.datalayer.Appointment;
 import edu.rit.rdi.as.datalayer.Security;
 import edu.rit.rdi.as.exceptions.DataLayerException;
+import edu.rit.rdi.as.services.messages.AbstractMessage;
 import edu.rit.rdi.as.services.messages.ErrorMessage;
 import edu.rit.rdi.as.services.messages.Message;
 import edu.rit.rdi.as.services.messages.NullMessage;
@@ -221,15 +222,49 @@ public class AppointmentHandler {
      * Change an appointment's time period, the doctor overseeing the appointment, or the patient who has the
      * appointment.
      * @param appointmentString Information string for an appointment. This string must be formatted like:
-     *                          ORIGINAL:Doctor_name,Patient_fName,Patient_lName,time|NEW:Doctor_name=[name]...
-     * @return A {@link Message} that represents whether this change was successful or not. {@link NullMessage} will
-     *         be passed back if there are no appointments found for the given doctor and patient, or if the appointment
-     *         is for a different patient. {@link ErrorMessage} will be passed back if an error occurred.
-     *         A valid {@link ServiceMessage} will have the following tags with the NEW information filled in:
-     *         "DOCTOR NAME", "PATIENT NAME", "TIME"
+     *                          <code>Doctor_id,Patient_id,time|Doctor_id,Patient_id,time,duration</code>
+     *                          where pre-delimiter is the original appointment data, and post-delimiter is new
+     *                          appointment data.
+     * @return A {@link Message} that represents whether this change was successful or not. 
+     *         <br/>{@link NullMessage} will be passed back if there are no appointments found for the given
+     *         doctor and patient, or if the appointment is for a different patient, or if the update was unsuccessful.
+     *         <br/>{@link ErrorMessage} will be passed back if an error occurred.
+     *         <br/>A valid {@link ServiceMessage} will have an Appointment tag with the NEW information filled in:
+     *         "APPOINTMENT_ID,PATIENT_ID,DOCTOR_ID,Year-Day-Month Hours:Minutes:Seconds,duration"
      */
     public Message changeAppointment( String appointmentString ) {
-        return null;
+        Message m = null;
+        //Split the parameter into OLD and NEW appointment strings.
+        String[] split = appointmentString.split( AbstractMessage.TAG_DELIM );
+        if( split.length > 2 ) {
+            m = new NullMessage();
+        } else {
+            try {
+                //Get the OLD appointment
+                Appointment oldAppointment = parseAppointment( split[0], false );
+                //Get NEW appointment values
+                Appointment newAppointment = parseAppointment( split[1], true );
+                //Set the new appointment's id number to the same as the old appointment.
+                newAppointment.setAppointmentId( oldAppointment.getAppointmentId() );
+                //Update the old appointment's values with the new appointment's values.
+                boolean success = newAppointment.post();
+                if( success ) {
+                    m = new ServiceMessage();
+                    m.setValue( APPOINTMENT, newAppointment.getAppointmentId()
+                                             + "," + newAppointment.getPatientId()
+                                             + "," + newAppointment.getDoctorId()
+                                             + "," + newAppointment.getDate()
+                                             + "," + newAppointment.getDuration() );
+                } else {
+                    m = new NullMessage();
+                }
+            } catch( DataLayerException dle ) {
+                m = new ErrorMessage();
+                m.setValue( ERROR, dle.getMessage() + "\n" + stackTraceAsString( dle ) );
+                m.setValue( DISPLAY_ERROR, "There was an error trying to update an appointment's information." );
+            }
+        }
+        return m;
     }
 
     /**
@@ -244,5 +279,37 @@ public class AppointmentHandler {
      */
     public Message deleteAppointment( String appointmentString ) {
         return null;
+    }
+
+    /**
+     * Parses an appointment string into an {@link Appointment} object.
+     * @param appointmentString The appointment string to parse an {@link Appointment} from. Valid appointment strings
+     *                          should only include the doctorId, the patientId, the date/time, and the duration, in
+     *                          that specific order and delimited by <code>,</code>. <code>appointmentString</code>
+     *                          does not have to include the duration.
+     * @param hasDuration Flag to say whether <code>appointmentString</code> has duration or not.
+     * @return An {@link Appointment} object after parsing <code>appointmentString</code>.
+     * @throws DataLayerException If <code>appointmentString</code> does not contain the correct amount of data, or
+     *                            if the doctor id, patient id, or duration are not numbers.
+     */
+    private Appointment parseAppointment( String appointmentString, boolean hasDuration ) throws DataLayerException {
+        try {
+            String[] appointmentData = appointmentString.split( "," );
+            int doctorId = Integer.parseInt( appointmentData[0] );
+            int patientId = Integer.parseInt( appointmentData[1] );
+            String day = appointmentData[2];
+            if( hasDuration ) {
+                int duration = Integer.parseInt( appointmentData[3] );
+                return new Appointment( doctorId, patientId, day, duration );
+            } else {
+                return new Appointment().fetchBySpecific( patientId, doctorId, day );
+            }
+        } catch( NumberFormatException nfe ) {
+            throw new DataLayerException( "There was a number in the appointment string at an incorrect location."
+                                          + "\n" + appointmentString, nfe );
+        } catch( ArrayIndexOutOfBoundsException oobe ) {
+            throw new DataLayerException( "The appointment string does not contain the correct amount of data."
+                                          + "\n" + appointmentString, oobe );
+        }
     }
 }
