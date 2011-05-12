dece06c4c7de3b11385ea2664cfb8e67c152c7a5
LunarZeal@gmail.com <LunarZeal@gmail.com@80e67094-1e62-00f2-85e5-5759996c6595>
2011-05-12 19:42:07 +0000

    Fixed LoginView to show another window. WeekView now requires credentials to be passed in (from LoginView, basically) and parses them into a userID and userType.

--- trunk/DesktopClient/Calender System/Calender System/WeekView.cs
+++ trunk/DesktopClient/Calender System/Calender System/WeekView.cs
@@ -7,13 +7,29 @@ using System.Linq;
 using System.Text;
 using System.Windows.Forms;
 
+using System.Text.RegularExpressions;
+
 namespace Calender_System
 {
     public partial class WeekView : Form
     {
-        public WeekView()
+        private String userID;
+        private String userType;
+        
+        public WeekView(String credentials)
         {
             InitializeComponent();
+            //String.split does not work so well when you want to split on a group of characters
+            String[] data = Regex.Split(credentials, "=>");
+            userID = data[1];
+            userType = data[0];
+            this.Show();
+            this.Focus();
+        }
+
+        private void WeekView_Load(object sender, EventArgs e)
+        {
+            //C# just needs this method for windows loaded after the first... Won't compile without it
         }
     }
 }
